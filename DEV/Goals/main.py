831bdb305abe4c74b11f45dc1e3f43cdb628bccc
haaland2907 <haaland2907@rediffmail.com>
2023-06-26 09:53:05 +0000

    added goal scorers

--- DEV/Goals/main.py
+++ DEV/Goals/main.py
@@ -1,1 +1,15 @@
 ########## GOALS ###########
+import json
+
+class EPL:
+
+    def print_goals():
+        print('1.       Erling Haaland Manchester City  36 (7)')
+        print('2.       Harry Kane      Tottenham Hotspur       30 (5)')
+        print('3.       Ivan Toney       Brentford FC   20 (6)')
+        print('4.       Mo Salah         Liverpool FC   19 (2)')
+        print('5.       Callum Wilson    Newcastle United       18 (3)')
+
+
+main = EPL
+main.print_goals()
